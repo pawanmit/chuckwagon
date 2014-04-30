75a377bddcbcf4b17d7cac47b12846ec998b634e
pawanmit <mitpawan@gmail.com>
2014-04-30 12:46:49 -0700

    adding column user_instructions

--- model/ddl.sql
+++ model/ddl.sql
@@ -7,7 +7,8 @@ DROP TABLE IF EXISTS user_response;
 CREATE TABLE response (
   id INT NOT NULL AUTO_INCREMENT,
   lunch_date DATE NOT NULL,
-  user_name varchar(255) NOT NULL,
+  user_name varchar(20) NOT NULL,
+  user_instructions varchar(100),
   user_response  BIT(1) DEFAULT 0,
   PRIMARY KEY (id),
   UNIQUE INDEX user_response_idx (lunch_date, user_name) 
