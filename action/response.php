d32aa00d7187b9949a5af555324da46e3bf08bec
pawanmit <mitpawan@gmail.com>
2014-04-30 10:48:29 -0700

    saving user instructions

--- action/response.php
+++ action/response.php
@@ -25,8 +25,9 @@ function submit_user_response() {
 }
 
 function save_user_response($response) {
-    $sql = "INSERT INTO response (user_response, user_name, lunch_date) VALUES ("
-                . $response->user_response . ",'" . $response->user_name . "', '" . $response->lunch_date
+    $sql = "INSERT INTO response (user_response, user_name, user_instructions, lunch_date) VALUES ("
+                . $response->user_response . ",'" . $response->user_name . "'," .
+                "'" . $response->user_instructions . "'," . "'" . $response->lunch_date
                 . "') ON DUPLICATE KEY UPDATE `user_response` = " . $response->user_response;
     $db_connection = wrapper_mysql_connect(null);
     //write_to_log($sql);
@@ -47,5 +48,6 @@ function create_response_object() {
     $response->user_response = $user_response_bit;
     $response->lunch_date = $lunch_date;
     $response->user_name = "$user_name";
+    $response->user_instructions = $instructions;
     return $response;
 }
